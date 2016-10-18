c7d1255289ecc907001e5e346238d9362b049abe
xyyx <xyyx@mail.ru>
2016-10-18 11:43:00 +0800

    Fix nitrogen prepare; Replace apns-conf.xml with stock NBD90Z

--- nitrogen_shamu.mk
+++ nitrogen_shamu.mk
@@ -19,7 +19,10 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/moto/shamu/apns-conf.xml:system/etc/apns-conf.xml
 
 # Inherit some common Nitrogen stuff.
 $(call inherit-product, vendor/nitrogen/products/common.mk)
