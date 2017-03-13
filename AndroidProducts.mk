1b25bd4375df7a142db2a3cf9b989a496f585c1b
nitin1438 <nitin.chobhe@gmail.com>
2017-03-13 11:21:11 +0530

    Update device trees

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2014 The Android Open Source Project
+# Copyright 2016 Nitrogen Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,4 +17,5 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_shamu.mk \
+    $(LOCAL_DIR)/nitrogen_shamu.mk \
     $(LOCAL_DIR)/bt_shamu.mk
