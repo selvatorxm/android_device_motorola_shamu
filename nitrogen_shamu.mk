nitin1438/device_moto_shamu

shamu: Add APN list

nitrogen_shamu.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_WIDTH := 1440
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Get the long list of APNs
-# PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+ PRODUCT_COPY_FILES := device/moto/shamu/apns-conf.xml:system/etc/apns-conf.xml
 
 # Inherit some common Nitrogen stuff.
 $(call inherit-product, vendor/nitrogen/products/common.mk)

selvatorxm/android_device_motorola_shamu

nitrogen_shamu.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_WIDTH := 1440
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Get the long list of APNs
-# PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES := device/motorola/shamu/apns-conf.xml:system/etc/apns-conf.xml
 
 # Inherit some common Nitrogen stuff.
 $(call inherit-product, vendor/nitrogen/products/common.mk)
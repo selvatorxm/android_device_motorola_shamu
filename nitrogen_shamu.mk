a11029c9a9c8bcf0b96bfa0c5276f1550217f920
nitin1438 <nitin.chobhe@gmail.com>
2017-12-01 21:08:52 +0530

    shamu: Add APN list

--- nitrogen_shamu.mk
+++ nitrogen_shamu.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_WIDTH := 1440
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Get the long list of APNs
-# PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES := device/motorola/shamu/apns-conf.xml:system/etc/apns-conf.xml
 
 # Inherit some common Nitrogen stuff.
 $(call inherit-product, vendor/nitrogen/products/common.mk)
